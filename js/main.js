d22dce924b459fb8a740a17dfbea17be802f7694
HuemanTarget <38532350+HuemanTarget@users.noreply.github.com>
2019-12-13 16:51:32 -0800

    added assets created simple html simple css started js

---
+++ js/main.js
@@ -0,0 +1,45 @@
+/*----- constants -----*/
+const deck = {
+    twohearts: {
+        value: 2,
+        img: "./assets/cards/hearts/2_of_hearts.png"
+    },
+    twospades: {
+        value: 2,
+        img: "./assets/cards/hearts/2_of_spades.png"
+    }
+}
+
+
+/*----- app's state (variables) -----*/
+
+
+
+/*----- cached element references -----*/
+
+
+
+
+/*----- event listeners -----*/
+
+
+
+
+/*----- functions -----*/
+
+/*initialize()
+   
+    function initialize() {
+    scores = {
+    p: 0,
+    c: 0,
+    t: 0,
+  }
+  
+  results = {
+    p: 'r',
+    c: 'r',
+  }
+  winner = null
+  render()
+} */
