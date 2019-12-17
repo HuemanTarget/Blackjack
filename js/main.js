8918488e0d4008cee67d2e505aaba94139f2bfc0
HuemanTarget <38532350+HuemanTarget@users.noreply.github.com>
2019-12-16 19:06:23 -0800

    fixed to be in compliance with MCV and added the the deal logic

--- js/main.js
+++ js/main.js
@@ -1,509 +1,245 @@
-/*----- constants -----*/
-/* const cards = {
-    twohearts: {
-        value: 2,
-        img: "./assets/cards/hearts/2_of_hearts.png"
-    },
-    twospades: {
-        value: 2,
-        img: "./assets/cards/spades/2_of_spades.png"
-    },
-    twoclubs: {
-        value: 2,
-        img: "./assets/cards/clubs/2_of_clubs.png"
-    },
-    twodiamonds: {
-        value: 2,
-        img: "./assets/cards/diamonds/2_of_diamonds.png"
-    },
-    threehearts: {
-        value: 3,
-        img: "./assets/cards/hearts/3_of_hearts.png"
-    },
-    threespades: {
-        value: 3,
-        img: "./assets/cards/spades/3_of_spades.png"
-    },
-    threeclubs: {
-        value: 3,
-        img: "./assets/cards/clubs/3_of_clubs.png"
-    },
-    threediamonds: {
-        value: 3,
-        img: "./assets/cards/diamonds/3_of_diamonds.png"
-    },
-    fourhearts: {
-        value: 4,
-        img: "./assets/cards/hearts/4_of_hearts.png"
-    },
-    fourspades: {
-        value: 4,
-        img: "./assets/cards/spades/4_of_spades.png"
-    },
-    fourclubs: {
-        value: 4,
-        img: "./assets/cards/clubs/4_of_clubs.png"
-    },
-    fourdiamonds: {
-        value: 4,
-        img: "./assets/cards/diamonds/4_of_diamonds.png"
-    },
-    fivehearts: {
-        value: 5,
-        img: "./assets/cards/hearts/5_of_hearts.png"
-    },
-    fivespades: {
-        value: 5,
-        img: "./assets/cards/spades/5_of_spades.png"
-    },
-    fiveclubs: {
-        value: 5,
-        img: "./assets/cards/clubs/5_of_clubs.png"
-    },
-    fivediamonds: {
-        value: 5,
-        img: "./assets/cards/diamonds/5_of_diamonds.png"
-    },
-    sixhearts: {
-        value: 6,
-        img: "./assets/cards/hearts/6_of_hearts.png"
-    },
-    sixspades: {
-        value: 6,
-        img: "./assets/cards/spades/6_of_spades.png"
-    },
-    sixclubs: {
-        value: 6,
-        img: "./assets/cards/clubs/6_of_clubs.png"
-    },
-    sixdiamonds: {
-        value: 6,
-        img: "./assets/cards/diamonds/6_of_diamonds.png"
-    },
-    sevenhearts: {
-        value: 7,
-        img: "./assets/cards/hearts/7_of_hearts.png"
-    },
-    sevenspades: {
-        value: 7,
-        img: "./assets/cards/spades/7_of_spades.png"
-    },
-    sevenclubs: {
-        value: 7,
-        img: "./assets/cards/clubs/7_of_clubs.png"
-    },
-    sevendiamonds: {
-        value: 7,
-        img: "./assets/cards/diamonds/7_of_diamonds.png"
-    },
-    eighthearts: {
-        value: 8,
-        img: "./assets/cards/hearts/8_of_hearts.png"
-    },
-    eightspades: {
-        value: 8,
-        img: "./assets/cards/spades/8_of_spades.png"
-    },
-    eightclubs: {
-        value: 8,
-        img: "./assets/cards/clubs/8_of_clubs.png"
-    },
-    eightdiamonds: {
-        value: 8,
-        img: "./assets/cards/diamonds/8_of_diamonds.png"
-    },
-    ninehearts: {
-        value: 9,
-        img: "./assets/cards/hearts/9_of_hearts.png"
-    },
-    ninespades: {
-        value: 9,
-        img: "./assets/cards/spades/9_of_spades.png"
-    },
-    nineclubs: {
-        value: 9,
-        img: "./assets/cards/clubs/9_of_clubs.png"
-    },
-    ninediamonds: {
-        value: 9,
-        img: "./assets/cards/diamonds/9_of_diamonds.png"
-    },
-    tenhearts: {
-        value: 10,
-        img: "./assets/cards/hearts/10_of_hearts.png"
-    },
-    tenspades: {
-        value: 10,
-        img: "./assets/cards/spades/10_of_spades.png"
-    },
-    tenclubs: {
-        value: 10,
-        img: "./assets/cards/clubs/10_of_clubs.png"
-    },
-    tendiamonds: {
-        value: 10,
-        img: "./assets/cards/diamonds/10_of_diamonds.png"
-    },
-    jackhearts: {
-        value: 10,
-        img: "./assets/cards/hearts/jack_of_hearts.png"
-    },
-    jackspades: {
-        value: 10,
-        img: "./assets/cards/spades/jack_of_spades.png"
-    },
-    jackclubs: {
-        value: 10,
-        img: "./assets/cards/clubs/jack_of_clubs.png"
-    },
-    jackdiamonds: {
-        value: 10,
-        img: "./assets/cards/diamonds/jack_of_diamonds.png"
-    },
-    queenhearts: {
-        value: 10,
-        img: "./assets/cards/hearts/queen_of_hearts.png"
-    },
-    queenspades: {
-        value: 10,
-        img: "./assets/cards/spades/queen_of_spades.png"
-    },
-    queenclubs: {
-        value: 10,
-        img: "./assets/cards/clubs/queen_of_clubs.png"
-    },
-    queendiamonds: {
-        value: 10,
-        img: "./assets/cards/diamonds/queen_of_diamonds.png"
-    },
-    kinghearts: {
-        value: 10,
-        img: "./assets/cards/hearts/king_of_hearts.png"
-    },
-    kingspades: {
-        value: 10,
-        img: "./assets/cards/spades/king_of_spades.png"
-    },
-    kingclubs: {
-        value: 10,
-        img: "./assets/cards/clubs/king_of_clubs.png"
-    },
-    kingdiamonds: {
-        value: 10,
-        img: "./assets/cards/diamonds/king_of_diamonds.png"
-    },
-    acehearts: {
-        value: 11,
-        value2: 1,
-        img: "./assets/cards/hearts/ace_of_hearts.png"
-    },
-    acespades: {
-        value: 11,
-        value2: 1,
-        img: "./assets/cards/spades/ace_of_spades.png"
-    },
-    aceclubs: {
-        value: 11,
-        value2: 1,
-        img: "./assets/cards/clubs/ace_of_clubs.png"
-    },
-    acediamonds: {
-        value: 11,
-        value2: 1,
-        img: "./assets/cards/diamonds/ace_of_diamonds.png"
-    }
-} */
-
 const cards = [
     {
-        name: 'twohearts',
         value: 2,
         img: "./assets/cards/hearts/2_of_hearts.png"
     },
     {
-        name: 'twospades',
         value: 2,
         img: "./assets/cards/spades/2_of_spades.png"
     },
     {
-        name: 'twoclubs',
         value: 2,
         img: "./assets/cards/clubs/2_of_clubs.png"
     },
     {
-        name: 'twodiamonds', 
         value: 2,
         img: "./assets/cards/diamonds/2_of_diamonds.png"
     },
     {
-        name: 'threehearts',
         value: 3,
         img: "./assets/cards/hearts/3_of_hearts.png"
     },
     {
-        name: 'threespades',
         value: 3,
         img: "./assets/cards/spades/3_of_spades.png"
     },
     {
-        name: 'threeclubs',
         value: 3,
         img: "./assets/cards/clubs/3_of_clubs.png"
     },
     {
-        name: 'threediamonds',
         value: 3,
         img: "./assets/cards/diamonds/3_of_diamonds.png"
     },
     {
-        name: 'fourhearts',
         value: 4,
         img: "./assets/cards/hearts/4_of_hearts.png"
     },
     {
-        name: 'fourspades',
         value: 4,
         img: "./assets/cards/spades/4_of_spades.png"
     },
     {
-        name: 'fourclubs',
         value: 4,
         img: "./assets/cards/clubs/4_of_clubs.png"
     },
     {
-        name: 'fourdiamonds',
         value: 4,
         img: "./assets/cards/diamonds/4_of_diamonds.png"
     },
     {
-        name: 'fivehearts',
         value: 5,
         img: "./assets/cards/hearts/5_of_hearts.png"
     },
     {
-        name: 'fivespades',
         value: 5,
         img: "./assets/cards/spades/5_of_spades.png"
     },
     {
-        name: 'fiveclubs',
         value: 5,
         img: "./assets/cards/clubs/5_of_clubs.png"
     },
     {
-        name: 'fivediamonds',
         value: 5,
         img: "./assets/cards/diamonds/5_of_diamonds.png"
     },
     {
-        name: 'sixhearts',
         value: 6,
         img: "./assets/cards/hearts/6_of_hearts.png"
     },
     {
-        name: 'sixspades',
         value: 6,
         img: "./assets/cards/spades/6_of_spades.png"
     },
     {
-        name: 'sixclubs',
         value: 6,
         img: "./assets/cards/clubs/6_of_clubs.png"
     },
     {
-        name: 'sixdiamonds',
         value: 6,
         img: "./assets/cards/diamonds/6_of_diamonds.png"
     },
     {
-        name: 'sevenhearts',
         value: 7,
         img: "./assets/cards/hearts/7_of_hearts.png"
     },
     {
-        name: 'sevenspades',
         value: 7,
         img: "./assets/cards/spades/7_of_spades.png"
     },
     {
-        name: 'sevenclubs',
         value: 7,
         img: "./assets/cards/clubs/7_of_clubs.png"
     },
     {
-        name: 'sevendiamonds',
         value: 7,
         img: "./assets/cards/diamonds/7_of_diamonds.png"
     },
     {
-        name: 'eighthearts',
         value: 8,
         img: "./assets/cards/hearts/8_of_hearts.png"
     },
     {
-        name: 'eightspades',
         value: 8,
         img: "./assets/cards/spades/8_of_spades.png"
     },
     {
-        name: 'eightclubs',
         value: 8,
         img: "./assets/cards/clubs/8_of_clubs.png"
     },
     {
-        name: 'eightdiamonds',
         value: 8,
         img: "./assets/cards/diamonds/8_of_diamonds.png"
     },
     {
-        name: 'ninehearts',
         value: 9,
         img: "./assets/cards/hearts/9_of_hearts.png"
     },
     {
-        name: 'ninespades',
         value: 9,
         img: "./assets/cards/spades/9_of_spades.png"
     },
     {
-        name: 'nineclubs',
         value: 9,
         img: "./assets/cards/clubs/9_of_clubs.png"
     },
     {
-        name: 'ninediamonds',
         value: 9,
         img: "./assets/cards/diamonds/9_of_diamonds.png"
     },
     {
-        name: 'tenhearts',
         value: 10,
         img: "./assets/cards/hearts/10_of_hearts.png"
     },
     {
-        name: 'tenspades',
         value: 10,
         img: "./assets/cards/spades/10_of_spades.png"
     },
     {
-        name: 'tenclubs',
         value: 10,
         img: "./assets/cards/clubs/10_of_clubs.png"
     },
     {
-        name: 'tendiamonds',
         value: 10,
         img: "./assets/cards/diamonds/10_of_diamonds.png"
     },
     {
-        name: 'jackhearts',
         value: 10,
         img: "./assets/cards/hearts/jack_of_hearts.png"
     },
     {
-        name: 'jackspades',
         value: 10,
         img: "./assets/cards/spades/jack_of_spades.png"
     },
     {
-        name: 'jackclubs',
         value: 10,
         img: "./assets/cards/clubs/jack_of_clubs.png"
     },
     {
-        name: 'jackdiamonds',
         value: 10,
         img: "./assets/cards/diamonds/jack_of_diamonds.png"
     },
     {
-        name: 'queenhearts',
         value: 10,
         img: "./assets/cards/hearts/queen_of_hearts.png"
     },
     {
-        name: 'queenspades',
         value: 10,
         img: "./assets/cards/spades/queen_of_spades.png"
     },
     {
-        name: 'queenclubs',
         value: 10,
         img: "./assets/cards/clubs/queen_of_clubs.png"
     },
     {
-        name: 'queendiamonds',
         value: 10,
         img: "./assets/cards/diamonds/queen_of_diamonds.png"
     },
     {
-        name: 'kinghearts',
         value: 10,
         img: "./assets/cards/hearts/king_of_hearts.png"
     },
     {
-        name: 'kingspades',
         value: 10,
         img: "./assets/cards/spades/king_of_spades.png"
     },
     {
-        name: 'kingclubs',
         value: 10,
         img: "./assets/cards/clubs/king_of_clubs.png"
     },
     {
-        name: 'kingdiamonds',
         value: 10,
         img: "./assets/cards/diamonds/king_of_diamonds.png"
     },
     {
-        name: 'acehearts',
         value: 11,
         value2: 1,
         img: "./assets/cards/hearts/ace_of_hearts.png"
     },
     {
-        name: 'acespades',
         value: 11,
         value2: 1,
         img: "./assets/cards/spades/ace_of_spades.png"
     },
     {
-        name: 'aceclubs',
         value: 11,
         value2: 1,
         img: "./assets/cards/clubs/ace_of_clubs.png"
     },
     {
-        name: 'acediamonds',
         value: 11,
         value2: 1,
         img: "./assets/cards/diamonds/ace_of_diamonds.png"
     }
 ];
-
-/* const cardsTwo = ['2h', '2']
-
-const debug = true; */
-
-let playedCards = [];
 let shuffledDeck = [...cards];
 
 
 /*----- app's state (variables) -----*/
 
-let playerScore, dealerScore, playerMoney
+let playerScore, dealerScore, playerMoney, playerBet, message, playerHand, dealerHand, cardsPlayed
 
 
 /*----- cached element references -----*/
 
+let pCardOne = document.querySelector('.cardone')
+let dCardOne = document.querySelector('.cardtwo')
+let pCardTwo = document.querySelector('.cardthree')
+let dCardTwo = document.querySelector('.cardfour')
+let pCardThree = document.querySelector('.cardfive')
+let pCardFour = document.querySelector('.cardsix')
+let pCardFive = document.querySelector('.cardseven')
+let dCardThree = document.querySelector('.cardeight')
+let dCardFour = document.querySelector('.cardnine')
+let dCardFive = document.querySelector('.cardten')
 
 
 /*----- event listeners -----*/
 
-document.getElementById('one').addEventListener('click', betOne)
+/* document.getElementById('one').addEventListener('click', betOne)
 document.getElementById('five').addEventListener('click', betFive)
 document.getElementById('twofive').addEventListener('click', betTwoFive)
-document.getElementById('onehun').addEventListener('click', betOneHun)
+document.getElementById('onehun').addEventListener('click', betOneHun) */
 document.getElementById('deal').addEventListener('click', deal)
 document.getElementById('hit').addEventListener('click', hit)
 document.getElementById('stand').addEventListener('click', stand)
@@ -518,8 +254,20 @@ function initialize() {
     playerMoney = 100
     dealerScore = 0
     playerScore = 0
+    pCardOne.img =
+    pCardTwo.img =
+    dCardOne.img =
+    dCardTwo.img =
+    playerHand = []
+    dealerHand = []
+    cardsPlayed = []
     shuffle();
-
+    message = ''
+    bjDrawMessage = ''
+    bjLoseMessage = ''
+    hsdMessage = ''
+    hsMessage = ''
+    render();
 }
 
 function shuffle() {
@@ -531,53 +279,98 @@ for(let i = shuffledDeck.length - 1; i > 0; i--) {
   }
 }
 
+console.table(shuffledDeck)
 
-function dealCards() {
+/* function bet() {
+    if (playerBet > playerMoney) {
+        return 'You do not have enough money to play!'
+    }
 
+} */
+
+/* function betOne() {
+    document.getElementById('message').innerHTML = 'You bet $1. Press Deal To Play!';
+    return document.getElementById('balance').innerHTML = playerMoney - 1
 }
 
-function player() {
-    document.getElementById('balance').innerHTML = playerMoney - 1
+function betFive() {
+    document.getElementById('message').innerHTML = 'You bet $5. Press Deal to Play!';
+    return document.getElementById('balance').innerHTML = playerMoney - 5
 }
 
+function betTwoFive() {
+    document.getElementById('message').innerHTML = 'You bet $25. Press Deal to Play!';
+    document.getElementById('balance').innerHTML = playerMoney - 25
+}
 
-    function betOne() {
-        document.getElementById('message').innerHTML = 'You bet $1. Press Deal To Play!';
-        document.getElementById('balance').innerHTML = playerMoney - 1
-    }
-
-    function betFive() {
-        document.getElementById('message').innerHTML = 'You bet $5';
-        document.getElementById('balance').innerHTML = playerMoney - 5
-    }
-
-    function betTwoFive() {
-        document.getElementById('message').innerHTML = 'You bet $25';
-        document.getElementById('balance').innerHTML = playerMoney - 25
-    }
-
-    function betOneHun() {
-        document.getElementById('message').innerHTML = 'You bet $100';
-        document.getElementById('balance').innerHTML = playerMoney - 100
-    }
-
+function betOneHun() {
+    document.getElementById('message').innerHTML = 'You bet $100. Press Deal to Play!';
+    document.getElementById('balance').innerHTML = playerMoney - 100
+} */
 
 function deal() {
-    let cardOne = document.querySelector('.cardone').src = shuffledDeck[0].img
-    let cardTwo = document.querySelector('.cardtwo').src = shuffledDeck[15].img
-    let cardThree = document.querySelector('.cardthree').src = cards[45].img
-    document.querySelector('.cardfour').src = './assets/cards/back_of_card.png'
-    document.getElementById('playerscore').innerHTML = cards[0].value + cards[45].value
+    let pCardOne = shuffledDeck.shift()
+    let dCardOne = shuffledDeck.shift()
+    let pCardTwo = shuffledDeck.shift()
+    let dCardTwo = shuffledDeck.shift()
+    playerHand = [pCardOne, pCardTwo]
+    dealerHand = [dCardOne, dCardTwo]
+    console.log(pCardOne.value, pCardTwo.value)
+    if(pCardOne.value + pCardTwo.value === 21 && dCardOne.value + dCardTwo.value != 21) {
+        message = 'You hit Blackjack and win!'
+    }else if(pCardOne.value + pCardTwo.value === 21 && dCardOne.value + dCardTwo.value === 21) {
+        message = 'You both hit Blackjack. Draw!'
+    }else if(pCardOne.value + pCardTwo.value != 21 && dCardOne.value + dCardTwo.value === 21) {
+        message = 'Dealer got Blackjack. You Lose!'
+    }else if(pCardOne.value + pCardTwo.value === 10 || pCardOne.value + pCardTwo.value === 11) {
+        message = 'Hit, Stand, or Double Down!'
+    }else{
+        message = 'Hit or Stand!'
+    }
+    render();
 }
 
-function hit() {
-    alert('button hit working')
-}
 
-function stand() {
-    alert('button stand working')
-}
+// function hit() {
+//     let pCardThree = shuffledDeck.shift().img
+//     let pCardFour = shuffledDeck.shift().img
+//     let pCardFive = shuffledDeck.shift().img
+//     playerHand = [pCardOne, pCardTwo, pCardThree, pCardFour, pCardFive]
+//     /* document.querySelector('.cardfive').src = shuffledDeck[4].img
+//     if(shuffledDeck[0].value + shuffledDeck[2].value + shuffledDeck[4].value > 21) {
+//         document.getElementById('playerscore').innerHTML = shuffledDeck[0].value + shuffledDeck[2].value + shuffledDeck[4].value
+//         return document.getElementById('message').innerHTML = 'You Busted! You Lose!'
+//     }else{
+//         document.getElementById('playerscore').innerHTML = shuffledDeck[0].value + shuffledDeck[2].value + shuffledDeck[4].value
+//         return document.getElementById('message').innerHTML = 'Hit or Stand!'
+//     }*/
+// }
+
+
+
+/* function stand() {
+    document.querySelector('.cardfour').src = shuffledDeck[3].img
+    document.getElementById('dealerscore').innerHTML = shuffledDeck[1].value + shuffledDeck[3].value
+    if(shuffledDeck[1].value + shuffledDeck[3].value <= 16) {
+        document.querySelector('.cardeight').src = shuffledDeck[5].img 
+        document.getElementById('dealerscore').innerHTML = shuffledDeck[1].value + shuffledDeck[3].value + shuffledDeck[5].img
+        document.querySelector('.cardnine').src = shuffledDeck[5].img 
+        document.getElementById('dealerscore').innerHTML = shuffledDeck[1].value + shuffledDeck[3].value + shuffledDeck[5].img
+} */
 
 function doubleDown() {
     alert('button Double Down working')
+}
+
+function render() {
+    // pCardOne.src = playerHand.length === 0 ? '' : playerHand[0].img;
+    pCardOne.src = playerHand[0].img
+    dCardOne.src = dealerHand[0].img
+    pCardTwo.src = playerHand[1].img
+    dCardTwo.src = dealerHand[1].img
+    /* pCardThree.src = playerHand[2]
+    pCardFour.src = playerHand[3]
+    pCardFive.src = playerHand[4] */
+    document.getElementById('message').innerHTML = message
+    // document.getElementById('playerscore').innerHTML = shuffledDeck[0].value + shuffledDeck[2].value
 }
